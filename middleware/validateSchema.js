be72c1e887d8eda136c245550ebed6e8d0a990c0
Tzouela Kontrasi <tzoela23@gmail.com>
2025-05-13 16:35:46 +0200

    in progress

--- middleware/validateSchema.js
+++ middleware/validateSchema.js
@@ -12,18 +12,15 @@ const validateFn = ajv.compile(schema);
 const validateSchema = (req, res, next) => {
   const { body } = req;
 
-  // Validate the request body against the JSON schema
   const isValid = validateFn(body);
 
   if (!isValid) {
-    // If validation fails, return a 400 Bad Request response with error details
     return res.status(400).json({
       message: 'Validation failed',
       errors: validateFn.errors,
     });
   }
-
-  // If validation passes, proceed to the next middleware or route handler
   next();
 };
+
 module.exports = validateSchema;
